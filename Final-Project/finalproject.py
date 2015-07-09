d8e8d0d77ff6f41c6e8a53cdf0e22a84cc785ecc
godfreyhobbs <godfreyhobbs@users.noreply.github.com>
2015-07-08 20:28:11 -0700

    Update finalproject.py
    adding read_input_file and print_data_matrix

--- Final-Project/finalproject.py
+++ Final-Project/finalproject.py
@@ -1,1 +1,23 @@
 #Insert code here
+__author__ = 'godfreyhobbs'
+
+import csv
+
+
+def read_input_files(filename, add_time_step=False):
+    counter = 0
+    result = []
+    with open(filename, 'rb') as csv_file:
+        my_reader = csv.reader(csv_file, delimiter=' ', quotechar='|')
+        for row in my_reader:
+            row_data = [int(x) for x in row[0].strip().split(",")]
+            if add_time_step:
+                result.append([counter] + row_data)
+                counter += 1
+            else:
+                result.append(row_data)
+    return result
+
+
+def print_data_matrix(data_matrix):
+    print('\n'.join([','.join(['{0}'.format(item) for item in row]) for row in data_matrix]))
