a7918c950e378c44e248047b36781802cb029115
godfreyhobbs <godfreyhobbs@gmail.com>
2015-07-10 15:53:57 -0700

    adding resource files for training.

--- Final-Project/finalproject.py
+++ Final-Project/finalproject.py
@@ -21,3 +21,13 @@ def read_input_files(filename, add_time_step=False):
 
 def print_data_matrix(data_matrix):
     print('\n'.join([','.join(['{0}'.format(item) for item in row]) for row in data_matrix]))
+
+# print read_input_files('/Users/godfreyhobbs/PycharmProjects/CS8803/Final-Project/Inputs/training_data.txt',True)[:200]
+first_two_hundred = read_input_files('/Users/godfreyhobbs/PycharmProjects/CS8803/Final-Project/Inputs/training_data.txt', True)[
+        :200]
+
+def unwind(data_array):
+    return data_array
+
+
+print unwind(first_two_hundred)
