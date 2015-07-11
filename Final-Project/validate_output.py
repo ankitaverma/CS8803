9178320def5a5c61dbca15758ba50020fa7c395e
godfreyhobbs <godfreyhobbs@gmail.com>
2015-07-11 14:45:05 -0700

    adding success and failure messages

--- Final-Project/validate_output.py
+++ Final-Project/validate_output.py
@@ -17,19 +17,23 @@ def test(in_data):
     negX = sum(n < 0 for n in allX)
     negY = sum(n < 0 for n in allY)
 
+    if Ncount == 60 and Xcount == 60 and Ycount == 60 and negX == 0 and negY == 0:
+        print "Sucess: no negative and sixty eneries"
+    else:
+        print "FAIL: something went wrong"
+        print "Number of data points: ", Ncount
+        print "Number of data points: ", Ncount
+        print "Number of X points: ", Xcount
+        print "Number of Y points: ", Ycount
+        print "Number of neg X points: ", negX
+        print "Number of neg Y points: ", negY
+
     return Ncount, Xcount, Ycount, negX, negY
 
 input_file = 'Inputs/training_data.txt'
 
 input_array =  np.genfromtxt(input_file, delimiter=',', dtype=int)
 
-Ncount, Xcount, Ycount, negX, negY = test(input_array)
-print "Number of data points: ", Ncount
-
-print "Number of X points: ", Xcount
-
-print "Number of Y points: ", Ycount
-print "Number of neg X points: ", negX
-
-print "Number of neg Y points: ", negY
+test(input_array[:60])
+test(input_array)
 
