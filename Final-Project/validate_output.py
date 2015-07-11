333ec876da70f9114df1c910404da6c5d91fe039
godfreyhobbs <godfreyhobbs@gmail.com>
2015-07-11 14:13:02 -0700

    replacing get input with built in csv reader.

--- Final-Project/validate_output.py
+++ Final-Project/validate_output.py
@@ -5,17 +5,6 @@ from math import *
 import numpy as np
 import csv
 
-def get_input(filename):
-    p = re.compile('[-0-9]+')
-
-    ia = []
-    f = open(filename, 'r')
-    for line in f:
-        x,y = p.findall(line)
-        ia.append([int(x),int(y)])
-    f.close()
-    return ia
-
 def test(in_data):
     allX = []
     allY = []
@@ -30,10 +19,9 @@ def test(in_data):
     return Ncount, Xcount, Ycount, negX, negY
 
 input_file = 'Inputs/training_data.txt'
-input_array = get_input(input_file)
 
-# reader=csv.reader(open(input_file,"rb"),delimiter=',')
-# input_array =list(reader)
+reader=csv.reader(open(input_file,"rb"),delimiter=',')
+input_array = list(reader)
 
 
 Ncount, Xcount, Ycount, negX, negY = test(input_array)
@@ -41,7 +29,7 @@ print "Number of data points: ", Ncount
 
 print "Number of X points: ", Xcount
 
-print "Number of Y points: ", Ycount 
+print "Number of Y points: ", Ycount
 print "Number of neg X points: ", negX
 
 print "Number of neg Y points: ", negY
