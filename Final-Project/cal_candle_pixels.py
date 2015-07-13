be58fa393dfa274c2079bb8036ff92b02f681592
godfreyhobbs <godfreyhobbs@gmail.com>
2015-07-12 22:28:26 -0700

    cleaned up candle data.

--- Final-Project/cal_candle_pixels.py
+++ Final-Project/cal_candle_pixels.py
@@ -10,11 +10,21 @@ MAX_Y = 974
 MIN_X = 240
 MAX_X = 1696
 
+CANDLE_EXTENT_MIN_X = 895 - MIN_X
+CANDLE_EXTENT_MAX_X = 1122 - MIN_X
+CANDLE_EXTENT_MIN_Y = 420 - MIN_Y
+CANDLE_EXTENT_MAX_Y = 656
+
 
 def cal_candle_pixels(data_array):
     num_y = MAX_Y - MIN_Y
     num_x = MAX_X - MIN_X
-    arena = np.ones((num_y, num_x), dtype=bool)
+    arena = np.zeros((num_y, num_x), dtype=bool)
+    # Turn on the in zone region
+    for curr_x in range(CANDLE_EXTENT_MIN_X, CANDLE_EXTENT_MAX_X+1):
+            for curr_y in range(CANDLE_EXTENT_MIN_Y, CANDLE_EXTENT_MAX_Y+1):
+                 arena[curr_y][curr_x] = True
+
     for row in data_array:
         x = row[0] - MIN_X
         y = row[1] - MIN_Y
@@ -22,17 +32,17 @@ def cal_candle_pixels(data_array):
         # do not worry about negatives as we only really care aobut the center
         for curr_x in range(x, x + 28):
             for curr_y in range(y, y + 14):
-                if curr_y < num_y and curr_x < num_x:
+                if CANDLE_EXTENT_MIN_Y <= curr_y <= CANDLE_EXTENT_MAX_Y and CANDLE_EXTENT_MIN_X <= curr_x <= CANDLE_EXTENT_MAX_X:
                     arena[curr_y][curr_x] = False
 
     # return arena[200:MAX_Y - 200, 200:MAX_X - 200]
-    arena = np.transpose(np.nonzero(arena[200:-200,200:-200])) + [MIN_Y+200, MIN_X+200]
-    swap_cols(arena,0,1)
+    arena = np.transpose(np.nonzero(arena[200:-200, 200:-200])) + [MIN_Y + 200, MIN_X + 200]
+    swap_cols(arena, 0, 1)
     return arena
 
 
 def swap_cols(arr, frm, to):
-    arr[:,[frm, to]] = arr[:,[to, frm]]
+    arr[:, [frm, to]] = arr[:, [to, frm]]
 
 
 def print_data_matrix(data_matrix):
