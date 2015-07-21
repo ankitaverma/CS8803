62e4774066c0003871d795400af9ffadef745e71
godfreyhobbs <godfreyhobbs@gmail.com>
2015-07-20 20:36:13 -0700

    adding path calculation

--- Final-Project/collisions_detection.py
+++ Final-Project/collisions_detection.py
@@ -77,6 +77,7 @@ def print_data_matrix(data_matrix):
 
 
 # Naively detect collisions.  Look for a change in heading > than say 15 degrees
+# return path The path includes the starting and ending collision points.
 def cal_collision_pixles(data_array):
     result = []
     data_array_values = np.array(data_array)
@@ -116,11 +117,14 @@ def cal_collision_pixles(data_array):
                 thrash += 1
             else:
                 thrash = 0
+
                 paths.append(curr_path)
             max_thrash = max(max_thrash, thrash)
             max_steps_between_collisions = max(steps_between_collisions, max_steps_between_collisions)
             steps_between_collisions = 0
             curr_path = []
+            # //Should both paths include the collison point
+            curr_path.append(curr_xy)
             result.append(curr_xy)
         prev_heading = curr_heading
         # print get_angle((curr_x, curr_y), (prev_x, prev_y), (prev_prev_x, prev_prev_y)), \
@@ -130,40 +134,20 @@ def cal_collision_pixles(data_array):
         prev_x = curr_x
         prev_y = curr_y
 
-    print "max max_steps_between_collisions [", max_steps_between_collisions, "]"
-    print "max_thrash [", max_thrash, "]"
-    print_data_matrix(paths)
+    # print "max max_steps_between_collisions [", max_steps_between_collisions, "]"
+    # print "max_thrash [", max_thrash, "]"
+    # print_data_matrix(paths)
     return result, paths
 
 
-
-input_file = 'Inputs/training_data.txt'
-
-input_array = np.genfromtxt(input_file, delimiter=',', dtype=int)
-
-pixels,paths = cal_collision_pixles(input_array)
-print_data_matrix(pixels)
-
-
+#################################################
+# input_file = 'Inputs/training_data.txt'
 #
-# def cal_candle_pixels(data_array):
-#     num_y = MAX_Y - MIN_Y
-#     num_x = MAX_X - MIN_X
-#     arena = np.ones((num_y, num_x), dtype=bool)
-#     for row in data_array:
-#         x = row[0] - MIN_X
-#         y = row[1] - MIN_Y
-#         # clear a 7 by 7 matrix
-#         # do not worry about negatives as we only really care aobut the center
-#         for curr_x in range(x, x + 28):
-#             for curr_y in range(y, y + 14):
-#                 if curr_y < num_y and curr_x < num_x:
-#                     arena[curr_y][curr_x] = False
-#
-#     # return arena[200:MAX_Y - 200, 200:MAX_X - 200]
-#     arena = np.transpose(np.nonzero(arena[200:-200, 200:-200])) + [MIN_Y + 200, MIN_X + 200]
-#     swap_cols(arena, 0, 1)
-#     return arena
+# input_array = np.genfromtxt(input_file, delimiter=',', dtype=int)
+
+# pixels,paths = cal_collision_pixles(input_array)
+# print_data_matrix(pixels)
+#################################################
 
 
 def swap_cols(arr, frm, to):
