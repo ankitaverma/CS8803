045e1a5ac3a1d8a26de2fd46c967c05cd1fd6540
godfreyhobbs <godfreyhobbs@gmail.com>
2015-07-20 20:57:53 -0700

    adding path calculation

--- finalproject/split_L2Error.py
+++ finalproject/split_L2Error.py
@@ -1,5 +1,9 @@
 __author__ = 'ppatel'
 
+from math import *
+import numpy as np
+import csv
+
 def split(in_data):
     split80 = in_data[0:int(len(in_data)*0.8):1]
     split20 = in_data[int(len(in_data)*0.8):len(in_data):1]
@@ -18,14 +22,15 @@ def L2Error(actual_data, pred_data):
             predY.append(predFrames[i][1])
             actualX.append(actualFrames[i][0])
             actualY.append(actualFrames[i][1])
-            error = error + (actualX[i] - predX[i])**2 + (actualY[i] - predY[i])**2
+            error =+ (actualX[i] - predX[i])**2 + (actualY[i] - predY[i])**2
             error = sqrt(error)
     return predFrames, actualFrames, error
 
 
 
-input_file = 'test01.txt'
-input_array = get_input(input_file)
+input_file = 'Inputs/test01.txt'
+input_array = np.genfromtxt(input_file, delimiter=',', dtype=int)
+
 
 split80, split20 = split(input_array)
 predFrames, actualFrames, error = L2Error(input_array, input_array)
